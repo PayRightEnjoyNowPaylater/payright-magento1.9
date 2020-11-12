77c860e01ebfaf468abad46545248f9bbd9088fc
Brian Ng <brian.ng@payright.com.au>
2020-11-12 21:05:02 +1100

    [fix] restored index function

--- app/code/community/Payright/Payright/Block/Products.php
+++ app/code/community/Payright/Payright/Block/Products.php
@@ -1,4 +1,10 @@
 <?php
 
 class Payright_Payright_Block_Products extends Mage_Core_Block_Template {
+    /*
+     * DO NOT REMOVE!
+     */
+    public function index() {
+        // Do nothing
+    }
 }
