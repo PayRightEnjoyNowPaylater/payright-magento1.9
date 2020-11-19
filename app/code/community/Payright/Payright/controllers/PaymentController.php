a0945f0ca1dd29166fce99ceedda87aa284d9a90
Brian Ng <brian.ng@payright.com.au>
2020-11-19 12:27:40 +1100

    [fix]

--- app/code/community/Payright/Payright/controllers/PaymentController.php
+++ app/code/community/Payright/Payright/controllers/PaymentController.php
@@ -65,8 +65,8 @@ class Payright_Payright_PaymentController extends Mage_Core_Controller_Front_Act
                 'Mage_Core_Block_Template',
                 'payright',
                 array('template' => 'payright/redirect.phtml')
-            )->setData('builtappendpoint', $builtAppUrl)
-            ->setData('checkoutid', $initialiseTransaction['data']['checkoutId']);
+            )->setData('builtAppendPoint', $builtAppUrl)
+            ->setData('checkoutId', $initialiseTransaction['data']['checkoutId']);
 
             $this->getLayout()->getBlock('content')->append($block);
             $this->renderLayout();
