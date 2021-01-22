ac9dbaa3b1be4998c141d6c79ad0f6f6ae4aad40
Brian Ng <brian.ng@payright.com.au>
2021-01-22 14:08:20 +1100

    fix typo

--- app/code/community/Payright/Payright/controllers/PaymentController.php
+++ app/code/community/Payright/Payright/controllers/PaymentController.php
@@ -128,8 +128,10 @@ class Payright_Payright_PaymentController extends Mage_Core_Controller_Front_Act
         } else {
             // Payment was successful, so update the order's state, send order email and move to the success page
             $order = Mage::getModel('sales/order');
-            $order->loadByIncrementId();
+            $order->loadByIncrementId($resOrderId);
             $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true, 'Gateway has authorized the payment.');
+
+            error_log("Order Id:".$resOrderId, 0);
 
             // Set Payright details.
             $order->setPayrightPlanId($resPlanId);
